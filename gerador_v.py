bf2ded427d4c8fcbe4f7c4c5af33ac8a8aabfeed
Laudier st <55962333+Laudier2@users.noreply.github.com>
2020-03-25 12:05:29 -0300

    Update gerador_v.py

--- gerador_v.py
+++ gerador_v.py
@@ -5,29 +5,30 @@ import MSFV
 import WIFI
 from os import system
 
-rl = 'N'
-while rl == 'N':
+permite = 'N'
+while permite == 'N':
     system('\033[32m\033[1m')
     system('clear')
     system('figlet @@@ KHLS @@@')
     print('''$______________________\033[34mVERSÃO 1.0 ANO DE LANÇAMENTO 2019\033[0;0m\033[32m\033[1m___________________$\033[0;0m''')
     print('¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨')
-    d = int(input( '''\033[32m\033[1mESCOLHA UMA OPÇÃO ABAIXO!\033[0;0m
+    impute1 = int(input( '''\033[32m\033[1mESCOLHA UMA OPÇÃO ABAIXO!\033[0;0m
     (1)Gerador de Payload.
     (2)Hacker Wifi.
     (3)Sair!
 
     \033[32m\033[1mDIGITE UMA OPÇÃO: \033[0;0m'''))
-    if d == 1:
+    if impute1 == 1:
         MSFV.Payload_Msfvenon()
 
-    elif d == 2:
+    elif imput1 == 2:
         WIFI.Hacker_wireless()
 
     else:
         print('--------------------------------------------------------------------------')
         print('==================== VOCÊ ESCOLHEU \033[31mSAIR\033[0;0m DO PROGRAMA ======================\n')
 
-    rl = str(input('\033[33mVocê quer mesmo sair do programa\033[0;0m S/N: ')).upper()
+    permite = str(input('\033[33mVocê quer mesmo sair do programa\033[0;0m S/N: ')).upper()
     system('clear')
     print('\n$$$$$$$$$$$$$$$$$ \033[33m= Obrigado pela visita =\033[0;0m $$$$$$$$$$$$$$$$$$$$$\n')
+    
